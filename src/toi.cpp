151ef270eaa46f60dbfe4a6c92afe6fc08d81cd3
Nikhil Roy <nikhilroy.net@gmail.com>
2018-05-02 00:00:02 +0530

    adding toi

---
+++ src/toi.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//using namespace std;
+//void toi(int n, string beg, string aux, string end){
+//	if(n==1){
+//		cout<<beg<<"->"<<end<<endl;
+//	}
+//	else{
+//		toi(n-1,beg,end,aux);
+//		toi(1,beg,aux,end);
+//		toi(n-1,aux,beg,end);
+//	}
+//}
+//int main(){
+//	int n;
+//	cin>>n;
+//	string beg = "beg",aux = "aux",end = "end";
+//	toi(n,beg,aux,end);
+//	return 0;
+//}
