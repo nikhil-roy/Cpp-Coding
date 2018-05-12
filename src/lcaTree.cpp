de0858c6c9fe52e252f426061a43484fa9c8ff1e
Nikhil Roy <nikhilroy.net@gmail.com>
2018-05-13 01:03:14 +0530

    lca in binary tree

---
+++ src/lcaTree.cpp
@@ -0,0 +1,17 @@
+//Node * LCA(Node* root ,int n1 ,int n2 )
+//{
+//   if(!root)
+//   return NULL;
+//   if(root->data == n1 || root->data == n2)
+//    return root;
+//
+//    Node *left = LCA(root->left,n1,n2);
+//    Node *right = LCA(root->right,n1,n2);
+//
+//    if(left && right)
+//    return root;
+//    if(!left)
+//    return right;
+//    else
+//    return left;
+//}
