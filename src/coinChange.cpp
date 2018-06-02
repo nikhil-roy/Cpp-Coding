604689c16bad7091e5b2ef4a20c567cb5d532786
Nikhil Roy <nikhilroy.net@gmail.com>
2018-06-02 23:26:50 +0530

    coin change for max no of ways

---
+++ src/coinChange.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+int printWays(int arr[],int n,int v){
+    int res[v+1]={0};
+    res[0]=1;
+    for(int i=0;i<n;i++){
+        for(int j=arr[i];j<=v;j++){
+            res[j]+=res[j-arr[i]];
+            }
+        }
+    return res[v];
+}
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    int v;
+	    cin>>v;
+	    sort(arr,arr+n);
+	    cout<<printWays(arr,n,v)<<endl;
+	    
+	}
+	return 0;
+}
