2f2b05ab6b3af9850edd9d0f96e0ce542db6b375
Nikhil Roy <nikhilroy.net@gmail.com>
2018-06-06 08:24:42 +0530

    bubble ans selection sort

---
+++ src/selectionSort.cpp
@@ -0,0 +1,13 @@
+// void selectionSort(int arr[] , int n){
+// 	for(int i=0;i<n;i++){
+// 		int min = INT_MAX;
+// 		int pos = -1;
+// 		for(int j=i;j<n;j++){
+// 			if(arr[j]<min){
+// 				min = arr[j];
+// 				pos=j;
+// 			}			
+// 		}
+// 		if(pos>=0)	swap(arr[i],arr[pos]);
+// 	}
+// }
