c08c3d7088963003ee0ca5e5afc2462e238f4c72
nikhil-roy <nikhilroy.net@gmail.com>
2018-10-22 23:56:22 +0530

    array rotation added

---
+++ src/arrayRotation.cpp
@@ -0,0 +1,13 @@
+// #include <iostream>
+// #include<string.h>
+// #include<algorithm>
+// using namespace std;
+// int main(){
+// 	int arr[] = {1,2,3,4,5,6,7,8,9,10};
+// 	int k = 2 ;
+// 	reverse(arr,arr+(10-k));
+// 	reverse(arr+(10-k) , arr+10);
+// 	reverse(arr,arr+10);
+// 	for(int i=0;i<10;i++)	cout << arr[i] << " ";	
+// 	return 0;
+// }
