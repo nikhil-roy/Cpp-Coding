7fb3abb656ccaa2bc029501ef12b7e56babcd7f6
Nikhil Roy <nikhilroy.net@gmail.com>
2019-02-10 19:24:04 +0530

    fib code added

---
+++ src/fib.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+// using namespace std;
+// int fib(int n) {
+//     if(n==1 or n==2) return 1;
+//     else {
+//         int arr[n+1] = {0};
+//         arr[1] = 1;
+//         arr[2] = 1;
+//         for(int i=3;i<=n;i++){
+//             arr[i] = arr[i-1]+arr[i-2];
+//         }
+//         return arr[n];
+//     }
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     cout<<fib(n)<<endl;
+//     return 0;
+// }
